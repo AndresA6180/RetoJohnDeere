3eb15135f49ab97d8f1df91ccd712f488d762a92
AndresA6180 <a01284373@itesm.mx>
2022-10-19 18:28:37 -0500

    Commit de comentario

--- app/src/main/java/com/example/retojohndeere/MainActivity.kt
+++ app/src/main/java/com/example/retojohndeere/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.retojohndeere
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Comentario
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
